66e3dae58ccd80bb832585a0aa5cbcecf74a7ecf
Mochammad Naufal Septifiandi <septifiandinaufal@gmail.com>
2026-09-20 15:11:16 +0800

    Comment out react query and tanstack router devtools

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { createRootRoute } from '@tanstack/react-router'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { queryClient } from '../lib/queryClient'
 import { AppLayout } from '../components/layout/AppLayout'
 
